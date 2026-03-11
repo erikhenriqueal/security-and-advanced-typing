7e8ecab19ab9b2b9d5f221463b3685a3fd407c30
Erik Lima <erikhalima05@gmail.com>
2026-03-11 16:34:09 -0300

    fix: individualize and integrate role/name/email/password login schemas

--- packages/shared/src/auth.ts
+++ packages/shared/src/auth.ts
@@ -1,15 +1,25 @@
 import { z } from "zod";
 
+export const roleSchema = z.string().min(1, "Role is required");
+export const nameSchema = z
+  .string()
+  .min(2, "Username must be at least 2 characters long")
+  .max(50, "Username must be at most 50 characters long");
+export const emailSchema = z.email("Invalid email address");
+export const passwordSchema = z
+  .string()
+  .min(8, "Password must be at least 8 characters long")
+  .max(32, "Password must be at most 128 characters long")
+  .regex(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*])[A-Za-z\d!@#$%&*]+$/,
+    "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (!, @, #, $, %, &, *)",
+  );
+
 export const loginSchema = z.object({
-  email: z.email("Invalid email address"),
-  password: z
-    .string()
-    .min(8, "Password must be at least 8 characters long")
-    .max(32, "Password must be at most 128 characters long")
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*])[A-Za-z\d!@#$%&*]+$/,
-      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (!, @, #, $, %, &, *)",
-    ),
+  roleName: roleSchema,
+  name: nameSchema,
+  email: emailSchema,
+  password: passwordSchema,
 });
 
 export type LoginInput = z.infer<typeof loginSchema>;
